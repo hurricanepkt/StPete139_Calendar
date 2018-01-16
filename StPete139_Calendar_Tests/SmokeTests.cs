b7764c86be752da6b040922c3a4ca6c9e225d259
Mark Greenway <markgreenway@ligmarine.com>
2018-01-16 17:51:08 -0500

    tests

---
+++ StPete139_Calendar_Tests/SmokeTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StPete139_Calendar_Tests
+{
+    [TestClass]
+    public class SmokeTests
+    {
+        [TestMethod]
+        public void Smoke()
+        {
+           Assert.AreEqual(2, 1 + 1);
+        }
+    }
+}
