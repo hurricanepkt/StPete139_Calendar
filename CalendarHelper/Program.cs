02e25ccad9aaf25e2e3b5c9801c2295fc701e494
Mark Greenway <MarkGreenwayIT@LIGMarine.com>
2018-01-16 23:23:22 -0500

    Settings reading

--- CalendarHelper/Program.cs
+++ CalendarHelper/Program.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace CalendarHelper
 {
     public class Program
     {
+
+        public static IConfigurationRoot Configuration { get; set; }
+
         public static void Main(string[] args)
         {
+          
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
--- CalendarHelper/Startup.cs
+++ CalendarHelper/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 using CalendarHelper.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Net.Http.Headers;
@@ -14,11 +16,23 @@ namespace CalendarHelper
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
+          
         }
+
+        public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -32,11 +46,17 @@ namespace CalendarHelper
 
             app.Run(async (context) =>
             {
-                context.Response.ContentType = "text/calendar";
-                context.Response.Headers.Add("Content-Disposition", "inline; filename=demo.ical");
-                var blah = new CalendarCreator();
-                
-                await context.Response.WriteAsync(blah.CreateDemo());
+                var wizard = (string) Configuration["option1"];
+                //foreach (var wizard in wizards)
+                //{
+                    await context.Response.WriteAsync(wizard);
+                //}
+              
+                //context.Response.ContentType = "text/calendar";
+                //context.Response.Headers.Add("Content-Disposition", "inline; filename=demo.ical");
+                //var blah = new CalendarCreator();
+
+                //await context.Response.WriteAsync(blah.CreateDemo());
             });
         }
     }
